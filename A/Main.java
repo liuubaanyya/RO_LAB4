b5abd6a28706ddec71b7a9375597cd7f6051c137
Liubov Prykhodko <113430539+liuubaanyya@users.noreply.github.com>
2023-10-10 23:53:51 +0300

    Update Main.java

--- A/Main.java
+++ A/Main.java
@@ -1,1 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Database database = new Database();
 
+        Thread[] readers = new Thread[5];
+        Thread[] writers = new Thread[5];
+
+        for (int i = 0; i < 5; i++) {
+            readers[i] = new ReaderThread(database);
+            writers[i] = new WriterThread(database);
+            readers[i].start();
+            writers[i].start();
+        }
+    }
+}
